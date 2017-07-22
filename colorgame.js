7ef6ab20cfe22266c9b085cf467332b785b4b651
Max <sirmaxwellk@gmail.com>
2017-07-21 22:37:56 -0400

    update

--- colorgame.js
+++ colorgame.js
@@ -4,6 +4,29 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn= document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+	this.classList.add("selected");
+	hardBtn.classList.remove("selected");
+});
+hardBtn.addEventListener("click", function() {
+	this.classList.add("selected");
+	easyBtn.classList.remove("selected");
+});
+
+resetButton.addEventListener("click", function() {
+	colors = generatorRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -13,7 +36,9 @@ for(var i = 0; i < squares.length; i++){
 			var clickedColor = this.style.background;
 			if (clickedColor === pickedColor) {
 				message.textContent = "Correct!";
+				resetButton.textContent = "Player Again?";
 				changeColors(clickedColor);
+				h1.style.background = clickedColor;
 			} else {
 				this.style.backgroundColor = "#232323";
 				message.textContent = "Try Again!";
@@ -32,20 +57,17 @@ function pickColor() {
 	return colors[random];
 }
 
-fucntion generatorRandomColors(num) {
+function generatorRandomColors(num) {
 	var arr = []
-	for (var i = 0; i < num; i++) {
-		var r = Math.floor(Math.random * num + 1);
-		var g = Math.floor(Math.random * num + 1);
-		var b = Math.floor(Math.random * num + 1);
-		arr.push("rgb" + );
+	for(var i = 0; i < num; i++) {
+		arr.push(randomColor());
 	}
 	return arr;
 }
 
 function randomColor() {
-	var color = "rgb(";
-	Math.floor(Math.random() * 256);
-	for (var i = 0; i < 3;)
-	return color;
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
